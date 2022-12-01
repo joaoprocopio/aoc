99af689a98be03b554511a01f9d1e4498a564063
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-11-30 22:55:17 -0300

    wip

--- puzzles/01/main.py
+++ puzzles/01/main.py
@@ -1,4 +1,12 @@
 import os
 
 path = "/".join([os.path.dirname(__file__), "./input.txt"])
-text = open(path, "r").read()
+text = open(path, "r").read().split()
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    print(main())
