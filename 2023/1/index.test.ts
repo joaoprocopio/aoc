bc0d4b4ae05eb45647cce17568f0a37352546a41
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-12-01 22:27:37 -0300

    add tests, reduce nesting

--- 2023/1/index.test.ts
+++ 2023/1/index.test.ts
@@ -1,6 +1,6 @@
-import { sum } from ".."
+import { sum } from "."
 
-describe("2023/1", () => {
+describe("2023 - 1", () => {
   it("should sum 1 and 2", () => {
     expect(sum(1, 2)).toBe(3)
   })
