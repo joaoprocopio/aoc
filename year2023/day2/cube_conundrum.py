734ab23647d83108d2056f35a4b1157bcfd58b6e
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-04-08 23:53:02 -0300

    cube conondrum

---
+++ year2023/day2/cube_conundrum.py
@@ -0,0 +1,16 @@
+# https://adventofcode.com/2023/day/2
+from pathlib import Path
+
+
+def cube_conundrum():
+    file = Path(__file__).parent / "cube_conundrum.txt"
+    lines = open(file).read().splitlines()
+    total = 0
+
+    # do some thingy
+
+    print(total)
+
+
+if __name__ == "__main__":
+    cube_conundrum()
