171136b11674b36ee00f9a865212592e1727e1d0
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-12 01:34:41 -0300

    year 2024(day 1, 1-star): completed

--- year_2024/day_1/main.py
+++ year_2024/day_1/main.py
@@ -1,7 +1,34 @@
+import heapq
 from pathlib import Path
 
-dir = Path(__file__).parent
-input = dir / "sample.txt"
-file = open(input)
+dir_path = Path(__file__).parent
+file_path = dir_path / "input.txt"
 
-print(file.read())
+
+left_heap: list[int] = []
+right_heap: list[int] = []
+
+
+with open(file_path, "r") as file:
+    for line in file:
+        stripped_line: str = line.rstrip("\n")
+        left_value, right_value = stripped_line.split("   ")
+        left_heap.append(int(left_value))
+        right_heap.append(int(right_value))
+
+heapq.heapify(left_heap)
+heapq.heapify(right_heap)
+
+
+total_distance = 0
+
+while len(left_heap) > 0 and len(right_heap) > 0:
+    left_heap_smallest = heapq.heappop(left_heap)
+    right_heap_smallest = heapq.heappop(right_heap)
+
+    min_distance = min(left_heap_smallest, right_heap_smallest)
+    max_distance = max(left_heap_smallest, right_heap_smallest)
+    distance_diff = max_distance - min_distance
+    total_distance = total_distance + distance_diff
+
+print(total_distance)
