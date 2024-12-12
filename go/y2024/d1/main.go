97cb3c014337d58cb8a659acbb5fce04fb53de26
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-11 22:57:04 -0300

    input

--- go/y2024/d1/main.go
+++ go/y2024/d1/main.go
@@ -1,8 +1,46 @@
 // https://adventofcode.com/2024/day/1
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+)
 
 func main() {
-	fmt.Println("abc")
+	cwd, err := getcwd()
+
+	if err != nil {
+		panic(err)
+	}
+
+	path := filepath.Join(cwd, "./input.txt")
+	file, err := os.Open(path)
+
+	if err != nil {
+		panic("could not open this file")
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func getcwd() (string, error) {
+	_, file, _, ok := runtime.Caller(0)
+
+	if !ok {
+		return "", errors.New("could not get the executable path")
+	}
+
+	cwd := filepath.Dir(file)
+
+	return cwd, nil
 }
