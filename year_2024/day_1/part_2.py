c649706e5c151ba73f41a895dec17ce4a858f5a7
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-12 01:55:49 -0300

    cleanup

--- year_2024/day_1/part_2.py
+++ year_2024/day_1/part_2.py
@@ -6,9 +6,6 @@ file_path = dir_path / "input.txt"
 
 
 left_heap: list[int] = []
-left_heap_occurrences: dict[int, int] = {}
-
-right_heap: list[int] = []
 right_heap_occurrences: dict[int, int] = {}
 
 
@@ -21,28 +18,21 @@ with open(file_path, "r") as file:
         right_value: int = int(splitted_line[1])
 
         left_heap.append(left_value)
-        right_heap.append(right_value)
-
-        if left_value not in left_heap_occurrences:
-            left_heap_occurrences[left_value] = 0
 
         if right_value not in right_heap_occurrences:
             right_heap_occurrences[right_value] = 0
 
-        left_heap_occurrences[left_value] += 1
         right_heap_occurrences[right_value] += 1
 
 
 heapq.heapify(left_heap)
-heapq.heapify(right_heap)
 
 
 total_distance = 0
 
 
-while len(left_heap) > 0 and len(right_heap) > 0:
+while len(left_heap) > 0:
     left_heap_smallest = heapq.heappop(left_heap)
-    right_heap_smallest = heapq.heappop(right_heap)
 
     if left_heap_smallest not in right_heap_occurrences:
         continue
