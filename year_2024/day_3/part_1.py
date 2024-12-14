68fbbe6e32fdc4b956954d69c3ef05e8043ad57d
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-14 00:02:57 -0300

    tokenizing

--- year_2024/day_3/part_1.py
+++ year_2024/day_3/part_1.py
@@ -1,20 +1,31 @@
+import tokenize
+from collections import deque
 from pathlib import Path
-from tokenize import tokenize
 
 dir_path = Path(__file__).parent
 file_path = dir_path / "sample.txt"
 
 
-MUL: str = "mul"
-OPEN_PAREN: str = "("
-CLOSED_PAREN: str = ")"
-COMMA: str = ","
+# mul   = len(3)        3
+# (     = len(1)        4
+# X     = len(1 -> 3)   7
+# ,     = len(1)        8
+# Y     = len(1 -> 3)   11
+# )     = len(1)        12
 
-MUL_OPEN_PAREN = MUL + OPEN_PAREN
-MUL_OPEN_PAREN_LEN = len(MUL) + len(OPEN_PAREN)
-
-with open(file_path, "rb") as file:
-    tokens = tokenize(file.readline)
+with tokenize.open(file_path) as file:
+    tokens = tokenize.generate_tokens(file.readline)
+    stack = deque()
 
     for token in tokens:
-        print(token.string)
+        match token:
+            case tokenize.TokenInfo(type=tokenize.NAME, string="mul"):
+                print(token)
+            case tokenize.TokenInfo(type=tokenize.NUMBER):
+                pass
+            case tokenize.TokenInfo(type=tokenize.OP, string="("):
+                pass
+            case tokenize.TokenInfo(type=tokenize.OP, string=","):
+                pass
+            case tokenize.TokenInfo(type=tokenize.OP, string=")"):
+                pass
