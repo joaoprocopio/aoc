92db223d8a0304754ab97f13588027935d101376
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-14 00:59:21 -0300

    vai

--- year_2024/day_3/part_1.py
+++ year_2024/day_3/part_1.py
@@ -5,16 +5,17 @@ from pathlib import Path
 dir_path = Path(__file__).parent
 file_path = dir_path / "input.txt"
 
-MUL_SYMBOL: str = "MUL"
-L_PAREN_SYMBOL: str = "L_PAREN"
-X_VALUE_SYMBOL: str = "X_VALUE"
-COMMA_SYMBOL: str = "COMMA"
-Y_VALUE_SYMBOL: str = "Y_VALUE"
-R_PAREN_SYMBOL: str = "R_PAREN"
+
+MUL_SYMBOL = "MUL"
+L_PAREN_SYMBOL = "L_PAREN"
+X_VALUE_SYMBOL = "X_VALUE"
+COMMA_SYMBOL = "COMMA"
+Y_VALUE_SYMBOL = "Y_VALUE"
+R_PAREN_SYMBOL = "R_PAREN"
 
 
 @dataclass
-class MulInstruction:
+class MUL_INSTRUCTION:
     MUL: str
     L_PAREN: str
     X_VALUE: int
@@ -23,15 +24,8 @@ class MulInstruction:
     R_PAREN: str
 
 
-# mul   = len(3)        3
-# (     = len(1)        4
-# X     = len(max. 3)   7
-# ,     = len(1)        8
-# Y     = len(max. 3)   11
-# )     = len(1)        12
-
-with tokenize.open(file_path) as file:
-    tokens = tokenize.generate_tokens(file.readline)
+with open(file_path, "rb") as file:
+    tokens = tokenize.tokenize(file.readline)
     total = 0
 
     instructions = dict()
