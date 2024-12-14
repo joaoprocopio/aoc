0396fd89b72fc41cf886a47627a63dd516f4eba8
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-14 01:26:55 -0300

    deu ruim

--- year_2024/day_3/part_1.py
+++ year_2024/day_3/part_1.py
@@ -3,15 +3,7 @@ from dataclasses import dataclass
 from pathlib import Path
 
 dir_path = Path(__file__).parent
-file_path = dir_path / "input.txt"
-
-
-MUL_SYMBOL = "MUL"
-L_PAREN_SYMBOL = "L_PAREN"
-X_VALUE_SYMBOL = "X_VALUE"
-COMMA_SYMBOL = "COMMA"
-Y_VALUE_SYMBOL = "Y_VALUE"
-R_PAREN_SYMBOL = "R_PAREN"
+file_path = dir_path / "sample.txt"
 
 
 @dataclass
@@ -28,17 +20,29 @@ with open(file_path, "rb") as file:
     tokens = tokenize.tokenize(file.readline)
     total = 0
 
-    instructions = dict()
+    instruction = dict()
 
     for token in tokens:
         match token:
             case tokenize.TokenInfo(type=tokenize.NAME) if token.string.endswith("mul"):
-                pass
+                instruction["MUL"] = "mul"
             case tokenize.TokenInfo(type=tokenize.NUMBER):
-                pass
+                if instruction.get("COMMA") == ",":
+                    instruction["Y_VALUE"] = int(token.string)
+                else:
+                    instruction["X_VALUE"] = int(token.string)
             case tokenize.TokenInfo(type=tokenize.OP, string="("):
-                pass
+                instruction["L_PAREN"] = "("
             case tokenize.TokenInfo(type=tokenize.OP, string=","):
-                pass
+                instruction["COMMA"] = ","
             case tokenize.TokenInfo(type=tokenize.OP, string=")"):
-                pass
+                instruction["R_PAREN"] = ")"
+
+                try:
+                    instruction = MUL_INSTRUCTION(**instruction)
+                    total += instruction.X_VALUE * instruction.Y_VALUE
+                    print(instruction)
+                    instruction = dict()
+                except Exception:
+                    instruction = dict()
+    print(total)
