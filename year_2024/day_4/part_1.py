7be7530a8e0ce9350454220d77d471e58828911d
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-15 00:05:16 -0300

    vai

--- year_2024/day_4/part_1.py
+++ year_2024/day_4/part_1.py
@@ -8,54 +8,6 @@ xmastrix = []
 xmascount = 0
 
 
-XMAS_HORIZONTAL = [
-    ["X", "M", "A", "S"],
-]
-XMAS_HORIZONTAL_REVERSE = [
-    ["S", "A", "M", "X"],
-]
-
-XMAS_VERTICAL = [
-    ["X"],
-    ["M"],
-    ["A"],
-    ["S"],
-]
-XMAS_VERTICAL_REVERSE = [
-    ["S"],
-    ["A"],
-    ["M"],
-    ["X"],
-]
-
-
-XMAS_DIAGONAL_AB = [
-    [None, None, None, "X"],
-    [None, None, "M"],
-    [None, "A"],
-    ["S"],
-]
-XMAS_DIAGONAL_AB_REVERSE = [
-    [None, None, None, "S"],
-    [None, None, "A"],
-    [None, "M"],
-    ["X"],
-]
-
-XMAS_DIAGONAL_CD = [
-    ["X"],
-    [None, "M"],
-    [None, None, "A"],
-    [None, None, None, "S"],
-]
-XMAS_DIAGONAL_CD_REVERSE = [
-    [None, None, None, "S"],
-    [None, None, "A"],
-    [None, "M"],
-    ["X"],
-]
-
-
 with open(file_path, "r") as file:
     for line in file:
         xmastrix.append([char for char in line.rstrip("\n")])
