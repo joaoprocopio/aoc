c2cc4dc1c1b28879d339969f6753bb5053c9ecff
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-14 23:59:09 -0300

    vai

--- year_2024/day_4/part_1.py
+++ year_2024/day_4/part_1.py
@@ -4,10 +4,60 @@ dir_path = Path(__file__).parent
 file_path = dir_path / "sample.txt"
 
 
-XMASTRIX = []
+xmastrix = []
+xmascount = 0
+
+
+XMAS_HORIZONTAL = [
+    ["X", "M", "A", "S"],
+]
+XMAS_HORIZONTAL_REVERSE = [
+    ["S", "A", "M", "X"],
+]
+
+XMAS_VERTICAL = [
+    ["X"],
+    ["M"],
+    ["A"],
+    ["S"],
+]
+XMAS_VERTICAL_REVERSE = [
+    ["S"],
+    ["A"],
+    ["M"],
+    ["X"],
+]
+
+
+XMAS_DIAGONAL_AB = [
+    [None, None, None, "X"],
+    [None, None, "M"],
+    [None, "A"],
+    ["S"],
+]
+XMAS_DIAGONAL_AB_REVERSE = [
+    [None, None, None, "S"],
+    [None, None, "A"],
+    [None, "M"],
+    ["X"],
+]
+
+XMAS_DIAGONAL_CD = [
+    ["X"],
+    [None, "M"],
+    [None, None, "A"],
+    [None, None, None, "S"],
+]
+XMAS_DIAGONAL_CD_REVERSE = [
+    [None, None, None, "S"],
+    [None, None, "A"],
+    [None, "M"],
+    ["X"],
+]
 
 
 with open(file_path, "r") as file:
     for line in file:
-        XMASTRIX.append([char for char in line.rstrip("\n")])
-    print(XMASTRIX)
+        xmastrix.append([char for char in line.rstrip("\n")])
+
+    print(xmascount)
