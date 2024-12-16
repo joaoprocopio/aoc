08c1da52d7424aa1781a1e67e37f5f25dacb81ed
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-16 20:55:34 -0300

    refact

--- year_2024/day_4/part_1.py
+++ year_2024/day_4/part_1.py
@@ -47,25 +47,32 @@ with open(file_path, "r") as file:
     matrix_rows_len = len(matrix)
     matrix_cols_len = len(matrix[0])
 
-    rows_index = 0
+    row_index = 0
 
-    while rows_index < matrix_rows_len:
-        cols_index = 0
+    while row_index < matrix_rows_len:
+        col_index = 0
 
-        while cols_index < matrix_cols_len:
-            char = matrix[rows_index][cols_index]
+        while col_index < matrix_cols_len:
+            char = matrix[row_index][col_index]
 
             if char == XMAS[0]:
-                vertical_match = [
-                    True
-                    for x_row_index, x_char_index, expected_char in XMAS_HORIZONTAL
-                    if rows_index + x_row_index < matrix_rows_len
-                    and cols_index + x_char_index < matrix_cols_len
-                    and expected_char
-                    == matrix[rows_index + x_row_index][cols_index + x_char_index]
-                ]
-                print(vertical_match)
+                for next_row_index, next_col_index, desired_char in XMAS_HORIZONTAL:
+                    desired_row_index = row_index + next_row_index
+                    desired_col_index = col_index + next_col_index
 
-            cols_index += 1
+                    if not (
+                        desired_row_index < matrix_rows_len
+                        and desired_col_index < matrix_cols_len
+                    ):
+                        break
 
-        rows_index += 1
+                    curr_char = matrix[desired_row_index][desired_col_index]
+
+                    if not curr_char == desired_char:
+                        break
+
+                    print(curr_char)
+
+            col_index += 1
+
+        row_index += 1
