9edb9e3e132d91b98ca7f26b5727914c33ad38b6
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-18 20:32:21 -0300

    vai

--- year_2024/day_4/part_1.py
+++ year_2024/day_4/part_1.py
@@ -1,10 +1,8 @@
 from pathlib import Path
-from typing import Optional
 
 dir_path = Path(__file__).parent
 file_path = dir_path / "sample.txt"
 
-abc = Optional
 
 count = 0
 
@@ -21,25 +19,6 @@ DIRECTIONS = []
 ( 1, -1) | ( 1,  0) | ( 1, 1)
 ( 0, -1) | ( 0,  0) | ( 0, 1)
 (-1, -1) | (-1,  0) | (-1, 1)
-"""
-
-
-"""
-se encontrar um X:
-- horizontal
-    - posição (0, 0) é igual a X
-    - posição (0, 1) é igual a M
-    - posição (0, 2) é igual a A
-    - posição (0, 3) é igual a S
-- vertical
-    - posição (0, 0) é igual a X
-    - posição (1, 0) é igual a M
-    - posição (2, 0) é igual a A
-    - posição (3, 0) é igual a S
-- diagonal 45°
-- diagonal 135°
-- diagonal 225°
-- diagonal 315°
 """
 
 
